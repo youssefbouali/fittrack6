f28e259f56c619b7f6a62d600f9e33768b05059a
Bouali <youssefbouali206@gmail.com>
2025-11-10 02:11:37 +0100

    Up

--- terraform/merged.tf
+++ terraform/merged.tf
@@ -306,7 +306,7 @@ resource "aws_security_group" "rds" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
     #security_groups = [aws_security_group.elastic_beanstalk.id]
   }
 
