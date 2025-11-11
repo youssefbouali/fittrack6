2580b3d892cebdd64e2f2a3b08a40150df84d373
Bouali <youssefbouali206@gmail.com>
2025-11-11 20:48:30 +0100

    Up

--- terraform/merged.tf
+++ terraform/merged.tf
@@ -615,20 +615,24 @@ resource "aws_cloudfront_distribution" "frontend" {
 
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.frontend.id
+
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      #Sid       = "AllowCloudFront"
-      Effect    = "Allow"
-      #Principal = { Service = "cloudfront.amazonaws.com" }
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.frontend.arn}/*"
-      #Condition = { StringEquals = { "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn } }
-    }]
+    Statement = [
+      {
+        Sid = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.frontend.id}"
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
   })
   depends_on = [aws_cloudfront_distribution.frontend]
 }
+
 
 # ========================================
 # Outputs
